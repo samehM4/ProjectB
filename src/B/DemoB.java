77b07faf94d307ac450c889874d58889be5fc5d6
samehmahfooz <samehmahfooz@macbook-pro-3>
2024-01-03 20:50:22 -0500

    Initial Commit

---
+++ src/B/DemoB.java
@@ -0,0 +1,14 @@
+package B;
+
+public class DemoB {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tuesday");
+		
+		System.out.println("Day1");
+		System.out.println("Day2");
+		
+		
+	}
+}
